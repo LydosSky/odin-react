5e087bc21d476f1e9fe3bb25c7afee08852108a0
Lydos <lydossky@gmail.com>
2024-11-21 21:37:48 +0300

    feat: newly created functional components

---
+++ src/components.jsx
@@ -0,0 +1,13 @@
+
+export function Header() {
+  return <h1>This is a header for the site</h1>
+}
+
+export function Main() {
+
+  return <main>This is a main for the site</main>
+}
+
+export function Footer() {
+  return <footer>This is the footer for the site</footer>
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Greeting from "./Greeting"
+import { Footer, Header, Main } from "./components";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Header />
+    <Main />
     <Greeting />
+    <Footer />
   </StrictMode>,
 );
 
