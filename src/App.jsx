799e636bfeedec481c6880691b60abfb9a760476
Lydos <lydossky@gmail.com>
2024-11-25 16:21:19 +0300

    chore: children prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import "./App.css";
 import { List } from "./Props"
 
+function Button({ text, color, fontSize }) {
+  const buttonStyle = {
+    color: color,
+    fontSize: fontSize + "px"
+  }
+  return (
+    <button style={buttonStyle}>{text}</button>
+  )
+}
+
+
 function App() {
   const animals = ["Lion", "Cow", "Snake", "Lizard"]
   return (
     <div>
-      <h1>Animals</h1>
-      <List animals={animals} />
+      <Button text="Click Me!" color="blue" fontSize={12} />
+      <Button text="Don't Click Me!" color="red" fontSize={12} />
+      <Button text="Click Me!" color="blue" fontSize={20} />
     </div>
   )
 }
