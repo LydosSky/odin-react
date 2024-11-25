84244f4a50c3d3396a944e9229ffea421b94f669
Lydos <lydossky@gmail.com>
2024-11-25 10:30:06 +0300

    chore: html to jsx conversion exercise

---
+++ src/conversion.jsx
@@ -0,0 +1,11 @@
+export function Conversion() {
+  return <>
+    <h1>Test title</h1>
+    <svg>
+      <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2"></circle>
+    </svg >
+    <form action=""><input type="text" /></form>
+  </>
+}
+
+
