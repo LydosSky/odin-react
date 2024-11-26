5a2c14e1dfca85ede49d0a1dc5bcf2fc5defdb84
Lydos <lydossky@gmail.com>
2024-11-26 21:53:29 +0300

    feat: ability to change person name and last name

--- src/Person.jsx
+++ src/Person.jsx
@@ -1,23 +1,34 @@
 import { useState } from "react";
 
 export default function Person() {
-  const [person, setPerson] = useState({ name: "John", age: 100 });
-  const [value, setValue] = useState("");
+  const [person, setPerson] = useState({
+    firstName: "john",
+    lastName: "doe",
+    age: 100,
+  });
 
-  const handleIncreaseAge = () => {
-    setPerson((prevPerson) => ({ ...prevPerson, age: person.age + 1 }));
-    setPerson((prevPerson) => ({ ...prevPerson, age: person.age + 1 }));
+  const changePerson = function (e) {
+    if (e.target.id === "firstName")
+      setPerson({ ...person, firstName: e.target.value });
+    if (e.target.id === "lastName")
+      setPerson({ ...person, lastName: e.target.value });
   };
 
   return (
     <>
-      <h1>{person.name}</h1>
+      <h1>{`${person.firstName} ${person.lastName}`}</h1>
       <h2>{person.age}</h2>
-      <button onClick={handleIncreaseAge}>Increase Age</button>
       <input
+        id="firstName"
         type="text"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
+        placeholder="FirstName"
+        onChange={(e) => changePerson(e)}
+      />
+      <input
+        id="lastName"
+        type="text"
+        placeholder="LastName"
+        onChange={(e) => changePerson(e)}
       />
     </>
   );
